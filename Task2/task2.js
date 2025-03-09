d9e048304ae0ed90a5aef469e6327bf0ecb5bfc5
Chashm-e-afreen <belta.infinita@yandex.com>
2025-03-09 23:10:25 +0500

    Add 3 variants of the function with a simple benchmark

--- Task2/task2.js
+++ Task2/task2.js
@@ -1,10 +1,46 @@
 const court = (name, numberOfJudges, others) => {
-  const time = (others.split(" ").filter(el => name > el).length) * 30 / numberOfJudges + 30;
-  console.log(time);
+  const time = ((others.split(" ").filter(el => name > el).length) * 30) / numberOfJudges + 30;
   return time;
 }
 
-const t0 = performance.now();
-let t = court("Zane", 1, "Mark Hank Ana Vivian")
-const t1 = performance.now();
-console.log(`This routine took ${t1-t0}ms to execute`);
+const court2 = (name, numberOfJudges, others) => {
+  let count = 0;
+  const othersArray = others.split(" ");
+  for (let o of othersArray) {
+    if (name > o) {
+      count+= 30
+    }
+  }
+  return count / numberOfJudges + 30;
+};
+
+const court3 = (name, numberOfJudges, others) => {
+  let count = 0;
+  let othersArray = others.split(" ")
+  for (let i = 0; i < othersArray.length; i++) {
+    if (name > othersArray[i]) {
+      count+= 30
+    }
+  }
+  return count / numberOfJudges + 30;
+};
+
+const benchmark = (func, name, numberOfJudges, others, iterations) => {
+    const t0 = performance.now()
+
+    for (let i = 0; i < iterations; i++) {
+        func(name, numberOfJudges, others)
+    }
+    const t = func(name, numberOfJudges, others)
+    console.log(t)
+    const t1 = performance.now()
+
+    return `${t1 - t0}ms`
+}
+
+const b1 = benchmark(court, "Jules", 3, "Adam Betty Frank Mike Adam Betty Frank Mike Adam Betty Frank Mike", 100000);
+console.log(b1)
+const b2 = benchmark(court2, "Jules", 3, "Adam Betty Frank Mike Adam Betty Frank Mike Adam Betty Frank Mike", 100000);
+console.log(b2)
+const b3 = benchmark(court3, "Jules", 3, "Adam Betty Frank Mike Adam Betty Frank Mike Adam Betty Frank Mike", 100000);
+console.log(b3)
